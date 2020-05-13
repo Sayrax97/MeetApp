9146d862ad0e28132825bd43554dcbd3784bccb2
Sayrax97 <dusann.jankovic@elfak.rs>
2020-05-13 22:38:01 +0200

    Strings added

--- app/app/src/main/java/com/team4infinity/meetapp/MainActivity.java
+++ app/app/src/main/java/com/team4infinity/meetapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.team4infinity.meetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
     }
 }
