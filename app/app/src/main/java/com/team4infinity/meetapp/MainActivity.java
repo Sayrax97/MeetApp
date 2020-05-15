4f0dea70c37a2fe5c6e112d525d9f85ae73a1a4f
Sayrax97 <dusann.jankovic@elfak.rs>
2020-05-15 12:24:36 +0200

    Map added

--- app/app/src/main/java/com/team4infinity/meetapp/MainActivity.java
+++ app/app/src/main/java/com/team4infinity/meetapp/MainActivity.java
@@ -1,19 +1,39 @@
 package com.team4infinity.meetapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.res.ResourcesCompat;
+
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
+public class MainActivity extends Activity {
+
+    private static final int PERMISSION_ACCESS_FINE_LOCATION = 1;
     private FirebaseAuth auth;
     private Context that=this;
+    private MapView map=null;
+    private GeoPoint myLocation;
+    private IMapController mapController=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +48,63 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+        Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        map = findViewById(R.id.map);
+        map.setMultiTouchControls(true);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ACCESS_FINE_LOCATION);
+        } else {
+        setMyLocationOverlay();
+        }
+        mapController=map.getController();
+        if(mapController!=null){
+            mapController.setZoom(15.0);
+            mapController.setCenter(new GeoPoint(43.3209,21.8958));
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+    private void setMyLocationOverlay(){
+        MyLocationNewOverlay myLocationNewOverlay=new MyLocationNewOverlay(new GpsMyLocationProvider(this),map);
+        myLocationNewOverlay.enableMyLocation();
+//        Drawable pointerDrawable= ResourcesCompat.getDrawable(getResources(),R.drawable.pointer,null);
+//        Bitmap pointerIcon=null;
+//        if(pointerDrawable!=null){
+//            pointerIcon=((BitmapDrawable)pointerDrawable).getBitmap();
+////            Drawable d=new BitmapDrawable(getResources(),Bitmap.createScaledBitmap(pointerIcon,75,75,true));
+////            pointerIcon=((BitmapDrawable)d).getBitmap();
+//            myLocationNewOverlay.setDirectionArrow( pointerIcon, pointerIcon );
+//            myLocationNewOverlay.setPersonIcon(pointerIcon);
+//        }
+        myLocation= myLocationNewOverlay.getMyLocation();
+        map.getOverlays().add(myLocationNewOverlay);
+        mapController = map.getController();
+        if (mapController != null) {
+            mapController.setZoom(15.0);
+            myLocationNewOverlay.enableFollowLocation();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_ACCESS_FINE_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    setMyLocationOverlay();
+                }
+                return;
+            }
+        }
     }
 }
